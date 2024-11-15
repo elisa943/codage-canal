9acd46eb5b55bed7efb4e5886a1483c11b1a7330
Alban Oberti <alban.oberti@bordeaux-inp.fr>
2024-11-15 09:31:43 +0100

    encode

---
+++ main.m
@@ -0,0 +1,10 @@
+% Clean 
+clear; close all; clc
+
+u=[1 1 0 1 0];
+trellis=poly2trellis(3,[7,5],7);
+c=cc_encode(u,trellis);
+
+ graph(1,1,:)=[0 20];
+    graph(1,2,:)=[graph(1,1,1)+poids(y,trellis.outputs(1,1)) 1];
+    graph(3,2,:)=[graph(1,1,1)+poids(y,trellis.outputs(1,2)) 1];
---
+++ viterbi_decode.m
@@ -0,0 +1,5 @@
+function v=viterbi_decode(y,trellis)
+    n=(length(y)/2)-2;
+    graph=zeros(8,length(y)/2);
+    % Ouverture
+end
